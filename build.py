a1a905158da4eb06db16a4fa15b9b0b23bb53fe1
Dalibor Frívaldský <dalibor.free@gmail.com>
2024-03-28 16:18:09 +0100

    Renamed variable 'format' to 'model_format' to avoid shadowing Python's 'format' function

--- build.py
+++ build.py
@@ -58,9 +58,9 @@ eye_end_fitting_part_right = _timeit(lambda: eye_end_fitting(
     hand='right'
 ).rotateAboutCenter((1, 0, 0), -90))
 
-format = 'step'
-build_dir = Path('.') / 'build' / format
+model_format = 'step'
+build_dir = Path('.') / 'build' / model_format
 build_dir.mkdir(parents=True, exist_ok=True)
-cq.exporters.export(body_part, str(build_dir / f'body.{format}'))
-cq.exporters.export(eye_end_fitting_part_left, str(build_dir / f'eye_end_fittng_left.{format}'))
-cq.exporters.export(eye_end_fitting_part_right, str(build_dir / f'eye_end_fittng_right.{format}'))
+cq.exporters.export(body_part, str(build_dir / f'body.{model_format}'))
+cq.exporters.export(eye_end_fitting_part_left, str(build_dir / f'eye_end_fittng_left.{model_format}'))
+cq.exporters.export(eye_end_fitting_part_right, str(build_dir / f'eye_end_fittng_right.{model_format}'))
